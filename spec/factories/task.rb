75937a30549603e6809783260ec77624e38828c5
Igor Amorim Silva <igoramorimsilva2@gmail.com>
2019-06-21 16:09:56 -0300

    add controller specs

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -4,8 +4,8 @@ require 'ffaker'
 FactoryBot.define do
   factory :task do
     body { FFaker::Lorem.sentence }
-    priority { (0..2).to_a.sample }
-    status { true }
+    priority { ['baixa', 'normal', 'alta'].sample }
+    status { 'ativa' }
     visible { true }
     association :user, factory: :user
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,18 +13,16 @@
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
-
-
 require 'rails_helper'
 
 describe User do
   describe 'validations' do
-    subject { user }
+    before(:each) do
+      @user = build(:user)
+    end
 
-    let(:user) { FactoryBot.build(:user) }
-
-    it { expect { user.email = nil }.to(change { user.valid? }.from(true).to(false)) }
-    it { expect { user.password = nil }.to(change { user.valid? }.from(true).to(false)) }
-    it { expect { user.password_confirmation = nil }.to(change { user.valid? }.from(true).to(false)) }
+    it { expect { @user.email = nil }.to(change { @user.valid? }.from(true).to(false)) }
+    it { expect { @user.password = nil }.to(change { @user.valid? }.from(true).to(false)) }
+    it { expect { @user.password_confirmation = 0 }.to(change { @user.valid? }.from(true).to(false)) }
   end
 end
